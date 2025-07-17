f9f05713e0e6aa054ceb7539435f8640907c1c43
ShadowStorm7894 <lachlan.jt.cook@gmail.com>
2025-07-18 00:36:56 +1000

    Commit: Update Recipes.js Date: 18/07/2025 12:37 AEST

--- Recipes.js
+++ Recipes.js
@@ -1,15 +1,15 @@
 ServerEvents.recipes(event => {
- event.shaped('minecraft:cobweb', 1),
-  [
-    'AAA',
-    'ABA',
-    'AAA'
-  ],
-  {
-    A: 'minecraft:string',
-    B: 'minecraft:slime_ball'
-  }
-
-
+ event.shaped(
+  Item.of('minecraft:cobweb', 1),
+   [
+     'AAA',
+     'ABA',
+     'AAA'
+   ],
+   {
+     A: 'minecraft:string',
+     B: 'minecraft:slime_ball'
+   }
+  )
  }
 )
