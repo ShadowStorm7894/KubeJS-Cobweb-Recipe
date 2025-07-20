1ef1f4f53b3133c62019bc144de1a6cb891828fc
ShadowStorm7894 <lachlan.jt.cook@gmail.com>
2025-07-21 07:11:48 +1000

    Commit: added new recipes Date: 21/7/2025 7:12 AEST

--- Recipes.js
+++ Recipes.js
@@ -10,6 +10,18 @@ ServerEvents.recipes(event => {
      A: 'minecraft:string',
      B: 'minecraft:slime_ball'
    }
+  ),
+  event.shaped(
+   Item.of('immersiveenginnering:raw_aluminium', 1),
+   [
+     'AB ',
+     'BA ',
+     '   '
+   ],
+   {
+     A: 'minecraft:raw_iron',
+     B: 'minecraft:redstone'
+   }
   )
  }
 )
