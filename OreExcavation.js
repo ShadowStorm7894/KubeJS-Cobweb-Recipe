5052851e0e1424aeaddbd6e7e689d28657eaae99
ShadowStorm7894 <lachlan.jt.cook@gmail.com>
2025-08-08 14:11:09 +1000

    Commit: refined OreExcavation Date:8/08/2025 14:11 AEST

--- OreExcavation.js
+++ OreExcavation.js
@@ -1,6 +1,6 @@
 ServerEvents.recipes(event => {
 	
-	function CustomVeinDrillingFluidnDrill(title, output, spacing, separation, salt, id, sizem, sizeM, dimension, drill, speed, stress) {
+	function CustomVeinDrillingFD(title, output, spacing, separation, salt, id, sizem, sizeM, dimension, speed, stress, fluid, drill) {
 		event.recipes.createoreexcavation.vein(`Raw ${title}`, output)
 		.placement(spacing, separation, salt)
 		.veinSize(sizem, sizeM)
@@ -9,7 +9,7 @@ ServerEvents.recipes(event => {
 
 		event.recipes.createoreexcavation.drilling(output, `kubejs:${id}`, speed)
 		.drill(`createoreexcavation:${drill}_drill`)
-		.fluid('tfmg:lubrication_oil 100')
+		.fluid(fluid)
 		.stress(stress)
 		.id(`kubejs:${id}_drilling`)
 	}
@@ -25,9 +25,34 @@ ServerEvents.recipes(event => {
 		.stress(stress)
 		.id(`kubejs:${id}_drilling`)
 	}
+	function CustomVeinDrillingF(title, output, spacing, separation, salt, id, sizem, sizeM, dimension, speed, stress, fluid) {
+		event.recipes.createoreexcavation.vein(`Raw ${title}`, output)
+		.placement(spacing, separation, salt)
+		.veinSize(sizem, sizeM)
+		.biomeWhitelist(`minecraft:is_${dimension}`)
+		.id(`kubejs:${id}`)
 
-	CustomVeinDrillingFluidnDrill('Aluminum', 'create_tank_defenses:raw_aluminum', 128, 8, 134567, 'raw_aluminum', 10, 30, 'overworld', 'diamond', 600, 256)
-	CustomVeinDrillingFluidnDrill('Titanium', 'create_tank_defenses:raw_titanium', 192, 64, 58768674, 'raw_titanium', 15, 40, 'overworld', 'diamond', 1200, 512)
+		event.recipes.createoreexcavation.drilling(output, `kubejs:${id}`, speed)
+		.drill('#createoreexcavation:drills')
+		.fluid(fluid)
+		.stress(stress)
+		.id(`kubejs:${id}_drilling`)
+	}
+	function CustomVeinDrillingD(title, output, spacing, separation, salt, id, sizem, sizeM, dimension, speed, stress, drill) {
+		event.recipes.createoreexcavation.vein(`Raw ${title}`, output)
+		.placement(spacing, separation, salt)
+		.veinSize(sizem, sizeM)
+		.biomeWhitelist(`minecraft:is_${dimension}`)
+		.id(`kubejs:${id}`)
+
+		event.recipes.createoreexcavation.drilling(output, `kubejs:${id}`, speed)
+		.drill(`createoreexcavation:${drill}_drill`)
+		.stress(stress)
+		.id(`kubejs:${id}_drilling`)
+	}
+
+	CustomVeinDrillingFD('Aluminum', 'create_tank_defenses:raw_aluminum', 128, 8, 134567, 'raw_aluminum', 10, 30, 'overworld', 600, 256, '','diamond')
+	CustomVeinDrillingFD('Titanium', 'create_tank_defenses:raw_titanium', 192, 64, 58768674, 'raw_titanium', 15, 40, 'overworld', 1200, 512, '','diamond')
     CustomVeinDrilling('Nickel', 'tfmg:raw_nickel', 128, 8, 987165, 'raw_nickel', 10, 30, 'overworld', 600, 256)
 	CustomVeinDrilling('Lithium', 'tfmg:raw_lithium', 128, 8, 6217165, 'raw_lithium', 10, 30, 'overworld', 600, 256)
 	CustomVeinDrilling('Lead', 'tfmg:raw_lead', 128, 8, 987165, 'raw_lead', 10, 30, 'overworld', 600, 256)
